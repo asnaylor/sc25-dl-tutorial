55403b0f180f21759a85b4cfc9534595ef42cc7d
Shashank Subramanian <shashank1693@gmail.com>
2025-10-08 20:03:20 -0700

    some extra flags

--- export_DDP_vars.sh
+++ export_DDP_vars.sh
@@ -2,3 +2,5 @@ export RANK=$SLURM_PROCID
 export LOCAL_RANK=$SLURM_LOCALID
 export WORLD_SIZE=$SLURM_NTASKS
 export MASTER_PORT=29500 # default from torch launcher
+export CUDA_DEVICE_MAX_CONNECTIONS=1 # TE suggests this for good comm overlap
+export NVTE_BATCH_MHA_P2P_COMM=1 # for CP in TE for unusual sizes
